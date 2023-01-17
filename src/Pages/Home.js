10705495cb0c3e710f3a0f09116d245af6acd730
sunnylead <amehsunday@gmail.com>
2023-01-17 15:28:24 +0100

    changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -86,6 +86,8 @@ const Home = () => {
         <img src={ss} width='60%' alt="logo" />
         <h2>cloning and git push update testing</h2>
         <h2>changing directory and git push update testing</h2>
+        <h2>NETLIFY</h2>
+
 </Col>
 
     <Col md>
